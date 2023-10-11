069a662cd4c42aa258307eb1a2c92f62bc6437f1
kalimelaswetha <swetha@truenil.io>
2023-10-11 15:37:51 +0530

    rempve user filters

--- src/components/HOCs/withFilters.js
+++ src/components/HOCs/withFilters.js
@@ -3,10 +3,8 @@ import React, { useState } from 'react';
 const withFilters = (WrappedComponent) => {
     const WithFilter = (props) => {
         const [filters, setFilters] = useState({});
-        const [page, setPage] = useState(1);
 
         const formSubmit = (data) => {
-            setPage(1);
             setFilters(data);
         };
 
@@ -14,15 +12,13 @@ const withFilters = (WrappedComponent) => {
 
         const btnList = [
             { text: "Filter" },
-          ];
+        ];
 
         const extra = {
             btnList,
             clearFilters,
             filters,
             formSubmit,
-            page,
-            setPage,
         };
 
         return <WrappedComponent {...props} {...extra} />
