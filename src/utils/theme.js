74a3cbdb226e9692bda24dea9d4207203991c768
mathan <mathan@gmail.com>
2023-11-01 09:20:16 +0000

    user api integrated

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,7 +1,7 @@
 export const colors = {
   black: "#000",
   white: "#fff",
-  primary: "#4154f1",
+  primary: "#012970",
   secondary: "#012970",
   primaryBg: "#4154f1",
   secondaryBg: "#f2f4f6",
