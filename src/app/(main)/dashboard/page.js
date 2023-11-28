6a0d8883acb07bfc50565f9b372dbf0fc7bd4f2c
mathan <mathan@gmail.com>
2023-11-28 22:32:35 +0000

    dashboard ui completed

--- src/app/(main)/dashboard/page.js
+++ src/app/(main)/dashboard/page.js
@@ -3,7 +3,7 @@
 import Table from "@/components/table";
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import { Select, MenuItem } from "@mui/material";
+import { Select, MenuItem, Card, CardContent } from "@mui/material";
 import { SAMPLE_COLUMNS } from "@/data/commonData";
 import { getService, apiList } from "@/utils";
 import { useRouter } from "next/navigation";
@@ -89,17 +89,142 @@ export default function Dashboard() {
   const deleteData = (data, e) => {};
 
   return (
-    <Table
-      pagination={getPaginationProps(
-        totalCount,
-        currentPage,
-        itemsPerPage,
-        handlePaginationChange
-      )}
-      columns={SAMPLE_COLUMNS}
-      data={data}
-      renderers={renderer}
-      onClickFns={{ deleteData }}
-    />
+    <>
+      <div className="mt-4 pt-2 ">
+        <h4 className="dashboard-head mb-4">Welcome to TrueNil!</h4>
+        <div className="d-flex gap-2 flex-wrap">
+          <div className="common-fill dashboard-card-item d-flex flex-column pb-4">
+            <h5 className="dashboard-title">Security Dashboard</h5>
+            <Card
+              sx={{ overflow: "visible" }}
+              className="common-card mt-0 text-center common-fill"
+            >
+              <CardContent className="d-flex gap-2 p-md-3 p-2 flex-wrap">
+                <div className="dashboard-box d-flex flex-column">
+                  <div className="d-flex m-1 m-md-2 gap-1 common-fill justify-content-evenly">
+                    <div className="dashboard-inner-box px-1">
+                      <p className="dashboard-inner-count pt-3">10</p>
+                      <p className="dashboard-inner-text">PII</p>
+                    </div>
+                    <div className="dashboard-inner-box px-1">
+                      <p className="dashboard-inner-count pt-3">4</p>
+                      <p className="dashboard-inner-text">PHI</p>
+                    </div>
+                    <div className="dashboard-inner-box px-1">
+                      <p className="dashboard-inner-count pt-3">40</p>
+                      <p className="dashboard-inner-text">PCI</p>
+                    </div>
+                    <div className="dashboard-inner-box px-1">
+                      <p className="dashboard-inner-count pt-3">60</p>
+                      <p className="dashboard-inner-text">Genomics</p>
+                    </div>
+                  </div>
+                  <p className="mb-2 mt-1">Sensitive Data Type</p>
+                </div>
+                <div className="dashboard-box d-flex flex-column">
+                  <div className="d-flex m-1 m-md-2 gap-1 common-fill justify-content-evenly">
+                    <div className="dashboard-inner-box px-1">
+                      <p className="dashboard-inner-count pt-3 common-danger">
+                        50
+                      </p>
+                      <p className="dashboard-inner-text">Without Encryption</p>
+                    </div>
+                    <div className="dashboard-inner-box px-1">
+                      <p className=" dashboard-inner-count pt-3 common-warning">
+                        5
+                      </p>
+                      <p className="dashboard-inner-text">
+                        Without Access Control
+                      </p>
+                    </div>
+                  </div>
+                  <p className="mt-1 mb-2">Open risks</p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+          <div className="common-fill dashboard-card-item  d-flex flex-column pb-4">
+            <h5 className="dashboard-title">Agent Status</h5>
+            <Card
+              sx={{ overflow: "visible" }}
+              className="common-card mt-0 text-center common-fill"
+            >
+              <CardContent className="d-flex gap-2 p-md-3 p-2 flex-wrap">
+                <div className="dashboard-box d-flex align-items-center flex-column px-2">
+                  <p className="dashboard-box-count my-4 pt-2 common-success">
+                    560
+                  </p>
+                  <p className="mb-3">Online</p>
+                </div>
+                <div className="dashboard-box d-flex align-items-center flex-column px-2">
+                  <p className="dashboard-box-count my-4 pt-2 common-danger">
+                    50
+                  </p>
+                  <p className="mb-3">Offline</p>
+                </div>
+                <div className="dashboard-box d-flex align-items-center flex-column px-2">
+                  <p className="dashboard-box-count my-4 pt-2 common-success">
+                    555
+                  </p>
+                  <p className="mb-3">Healthy Agents</p>
+                </div>
+                <div className="dashboard-box d-flex align-items-center flex-column px-2">
+                  <p className="dashboard-box-count my-4 pt-2 common-warning">
+                    43
+                  </p>
+                  <p className="mb-3">Compromised Agents</p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+        <div className="d-flex gap-2 flex-wrap">
+          <div className="common-fill dashboard-card-item  d-flex flex-column mb-4">
+            <h5 className="dashboard-title">User Management</h5>
+            <Card
+              sx={{ overflow: "visible" }}
+              className="common-card mt-0 common-fill text-center"
+            >
+              <CardContent className="d-flex gap-2 p-md-3 p-2 flex-wrap">
+                <div className="dashboard-box d-flex flex-column px-2">
+                  <p className="dashboard-box-count my-4 pt-2">43</p>
+                  <p className="mb-3">Admin</p>
+                </div>
+                <div className="dashboard-box d-flex flex-column px-2">
+                  <p className="dashboard-box-count my-4 pt-2">34</p>
+                  <p className="mb-3">Researcher</p>
+                </div>
+                <div className="dashboard-box d-flex flex-column px-2">
+                  <p className="dashboard-box-count my-4 pt-2">65</p>
+                  <p className="mb-3">Operator</p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+          <div className="common-fill dashboard-card-item  d-flex flex-column mb-4">
+            <h5 className="dashboard-title">Control Management</h5>
+            <Card
+              sx={{ overflow: "visible" }}
+              className="common-card mt-0 common-fill text-center"
+            >
+              <CardContent className="d-flex gap-2 p-md-3 p-2 flex-wrap">
+                <div className="dashboard-box d-flex flex-column px-2">
+                  <p className="dashboard-box-count my-4 pt-2 common-success">
+                    34
+                  </p>
+                  <p className="mb-3">Security Controls in Place</p>
+                </div>
+                <div className="dashboard-box d-flex flex-column px-2">
+                  <p className="dashboard-box-count my-4 pt-2 common-warning">
+                    0
+                  </p>
+                  <p className="mb-3">Security Controls Missing</p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
